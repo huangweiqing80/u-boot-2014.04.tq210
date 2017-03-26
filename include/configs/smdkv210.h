7d8c769bfb310a07ff08316fbeccc7a9d6a4bb1e
huangweiqing <706109460@qq.com>
2017-03-27 02:39:47 +0800

    git rm 删除忽略裁剪文件并添加编译工具选项

--- include/configs/smdkv210.h
+++ include/configs/smdkv210.h
@@ -32,10 +32,7 @@
 #define CONFIG_SYS_CLK_FREQ		12000000
 
 /* DRAM Base */
-#define CONFIG_SYS_SDRAM_BASE		0x20000000	/* modied by zjh */
-
-/* DRAM Base */
-#define CONFIG_SYS_SDRAM_BASE		0x30000000
+#define CONFIG_SYS_SDRAM_BASE		0x20000000	/* modied by hwq */
 
 /* Text Base */
 #define CONFIG_SYS_TEXT_BASE		0x34800000
